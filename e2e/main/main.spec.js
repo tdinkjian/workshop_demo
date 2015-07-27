5c838149af43d9783b9583744df71c122f6708e3
Melissa Kam <melissa.kam@rackspace.com>
2015-07-27 14:17:26 -0500

    Updating test to proper title

--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -11,7 +11,7 @@ describe('Main View', function() {
   });
 
   it('should include jumbotron with correct data', function() {
-    expect(page.h1El.getText()).toBe('\'Allo, \'Allo!');
+    expect(page.h1El.getText()).toBe('Upstream, Openstack Project');
     expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
     expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
   });
